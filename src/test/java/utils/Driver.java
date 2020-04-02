e47691b78910177bdd8e27b566c792098e47ed9c
Felipe-MDE-Endava <felipecorrea@Felipes-MacBook-Pro.local>
2020-04-02 15:43:19 -0500

    Second session code

---
+++ src/test/java/utils/Driver.java
@@ -0,0 +1,16 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+
+    public static WebDriver getDriver() {
+        System.setProperty("webdriver.chrome.driver", "/path/to/your/chromedriver");
+        WebDriver wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        return wd;
+    }
+}
